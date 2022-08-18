b4366948ec7dd3ae155e3bedce408de53a8431d9
Karam19 <karam.shbeb1@hotmail.com>
2022-08-19 00:43:49 +0300

    add Russian language

--- source/downloads.py
+++ source/downloads.py
@@ -1,7 +1,6 @@
 import requests
 import os
 import tarfile
-import nltk
 
 
 def download_model_en():
@@ -26,10 +25,23 @@ def download_model_fr():
         tar.close()
 
 
+def download_model_ru():
+    filename = 'ru_core_news_md_temporary'
+    if not os.path.exists(filename):
+        r = requests.get('https://github.com/explosion/spacy-models/releases/download/ru_core_news_md-3.4.0'
+                         '/ru_core_news_md-3.4.0.tar.gz', allow_redirects=True)
+        open(filename, 'wb').write(r.content)
+        tar = tarfile.open(filename, 'r:gz')
+        tar.extractall()
+        tar.close()
+
+
 def make_downloads(lang):
     if lang == 'eng':
         download_model_en()
     elif lang == 'fra':
         download_model_fr()
+    elif lang == 'ru':
+        download_model_ru()
     # else:
     #     print("Please choose one of the following languages: ['eng','fra']")
